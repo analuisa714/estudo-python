5f4a362c74552a0a00144118a7a093e10a38e810
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2023-06-05 13:03:01 -0300

    commit inicial

---
+++ src/01-intro/aula01.py
@@ -0,0 +1,14 @@
+""" aula-01 """
+
+print('olá mundo')
+
+
+def somar(numero_01, numero_02):
+    """ somar - soma dois números """
+    return numero_01 + numero_02
+
+
+soma = somar(10.3, 2.5)
+
+
+print(soma)
