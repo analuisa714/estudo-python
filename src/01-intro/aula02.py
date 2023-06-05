1371194707d0ed01a82e7dcc33f951592265be5e
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2023-06-05 13:24:37 -0300

    codigo de estudo aula 02

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,37 @@
+""" Aula 02 - Keywords e identificadores """
+
+# Keywords: Palavras reservadas
+# True, False, None, and, import, lambda
+
+# Identificadores
+# nomes de variáveis, funções, classes
+# case sensitive
+# devem iniciar com uma letra ou _
+# não pode ter espaços em branco
+# não pode usar caracteres especiais 
+
+nome = "Maria"
+idade = 30
+Nome = 'João'
+nome_completo = 'Maria da Silva'
+
+# Boas práticas (clean code)
+
+# nomes significativos para variáveis
+
+contador = 10
+
+# Constantes em maiúsculo
+
+PI = 3.14
+
+# Condições
+
+idade = 15
+
+MAIORIDADE = 18
+
+if idade >= MAIORIDADE:
+    print ("Maior de idade")
+else:
+    print ("Menor de idade")
