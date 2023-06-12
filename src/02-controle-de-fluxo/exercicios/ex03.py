7c1a31a91e1c406155a87c775de6c629acd8f59c
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2023-06-12 14:00:56 -0300

    exercício 03

---
+++ src/02-controle-de-fluxo/exercicios/ex03.py
@@ -0,0 +1,25 @@
+""" Exercício 03 - verificar identificador """
+
+identificador = input('Digite seu identificador: ')
+identificador = list(identificador)
+
+NUMERO_INVALIDO = True
+
+for i in range(2, 6):
+    if identificador[i].isdigit():
+        identificador[i] = int(identificador[i])
+        if (0 <= identificador[i] <= 9) and (i != 5):
+            NUMERO_INVALIDO = False
+        elif (1 <= identificador[i] <= 9) and (i == 5):
+            NUMERO_INVALIDO = False
+
+if len(identificador) != 7:
+    print('Identificador inválido')
+elif identificador[0] != 'B' or identificador[1] != 'R':
+    print('Identificador inválido')
+elif identificador[len(identificador) - 1] != 'X':
+    print('Identificador inválido')
+elif NUMERO_INVALIDO:
+    print('Identificador inválido')
+else:
+    print('Identificador válido')
