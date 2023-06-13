618323033bcb225896a7fc70edae5b625826ab6f
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2023-06-13 14:13:53 -0300

    arrumando nome da tupla

--- src/03-tipos-de-dados/exercicios/ex02.py
+++ src/03-tipos-de-dados/exercicios/ex02.py
@@ -1,6 +1,6 @@
 """ Exercício 02 - meses do ano com tupla """
 
-MESES = (
+meses = (
     'Janeiro', 
     'Fevereiro', 
     'Março', 
@@ -20,6 +20,6 @@ mes = int(input('Digite um mês do ano, em formato numérico: '))
 MES_VALIDO = 1 <= mes <= 12
 
 if MES_VALIDO:
-    print(MESES[mes-1])
+    print(meses[mes-1])
 else:
     print('Mês digitado inválido. O número deve ser de 1 a 12!')
