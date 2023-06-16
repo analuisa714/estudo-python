4f2f3f50671822ecd8ee47bd990c20db6fadb500
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2023-06-16 13:40:13 -0300

    exercício 01

---
+++ src/06-arquivos/exercicios/ex01.py
@@ -0,0 +1,23 @@
+""" Exercício 01 """
+
+def carregar_dados_alunos(nome_arquivo):
+    with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+        lista = []
+        for linha in arquivo:
+            dados = linha.split(sep=',')
+            dicionario = {
+                'prontuario': dados[0],
+                'nome': dados[1],
+                'email': dados[2]
+            }
+            lista.append(dicionario)
+    tupla = (lista)
+    return tupla
+   
+tupla_alunos = carregar_dados_alunos('src/06-arquivos/exercicios/dados_01.txt')
+
+for aluno in tupla_alunos:
+    print('{')
+    for key, value in aluno.items():
+        print(f'    {key} : {value}')
+    print('}')
