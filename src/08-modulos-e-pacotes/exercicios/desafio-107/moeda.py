3f83b93403f61335b9fde6e817e0d400e3935ac1
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2023-06-22 12:02:40 -0300

    tirando parenteses desnecessários

--- src/08-modulos-e-pacotes/exercicios/desafio-107/moeda.py
+++ src/08-modulos-e-pacotes/exercicios/desafio-107/moeda.py
@@ -5,7 +5,7 @@ def dobro(valor):
     return valor * 2
 
 def aumentar(valor, porcentagem):
-    return valor + ((porcentagem/100) * valor)
+    return valor + (porcentagem/100 * valor)
 
 def diminuir(valor, porcentagem):
-    return valor - ((porcentagem/100) * valor)
+    return valor - (porcentagem/100 * valor)
