a591ad01f3fcca2f5b12cf6532c3456df35dad84
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2023-06-22 11:41:55 -0300

    desafio 107

---
+++ src/08-modulos-e-pacotes/exercicios/desafio-107/moeda.py
@@ -0,0 +1,11 @@
+def metade(valor):
+    return valor / 2
+
+def dobro(valor):
+    return valor * 2
+
+def aumentar(valor, porcentagem):
+    return valor + ((porcentagem/100) * valor)
+
+def diminuir(valor, porcentagem):
+    return valor - ((porcentagem/100) * valor)
